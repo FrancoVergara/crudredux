572d56828433412c504030afbc104818904dfe12
Franco Vergara <franvergara96@gmail.com>
2020-11-14 09:38:04 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react';
-
+import React from 'react';
 import Header from './components/Header';
+import Products from './components/Products';
+import NewProducts from './components/NewProducts';
+import EditProducts from './components/EditProducts';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Fragment>
-
+    <Router>
       <Header/>
 
-    </Fragment>
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products}/>
+          <Route exact path="/products/new" component={NewProducts}/>
+          <Route exact path="/products/edit/:id" component={EditProducts}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NewProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewProducts = () => {
+    return (
+        <h1>Desde Nuevos Productos</h1>
+    );
+}
+ 
+export default NewProducts;
